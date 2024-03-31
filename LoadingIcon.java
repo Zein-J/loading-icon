ecbf6c9c73e511be04fb99f8c73f1d87d0529eb0
Zein J <165584221+Zein-J@users.noreply.github.com>
2024-03-31 14:08:41 -0400

    Adding code files

---
+++ LoadingIcon.java
@@ -0,0 +1,158 @@
+//Loading Icon - Zein Al-Jaradat
+package loadingIcon;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import net.miginfocom.swing.MigLayout;
+
+public class LoadingIcon implements ActionListener {
+
+	public static void main(String[] args) {
+		new LoadingIcon();
+	}
+
+	DrawingPanel dPanel;
+	JPanel panel;
+	JFrame win;
+	static int panW = 800;
+	static int panH = 800;
+
+	//					  (point 1 ,point 2)
+	//					   (x1 ,y1 ,x2 ,y2)
+	Line lineTop = new Line(220,200,580,200);
+	Line lineBot = new Line(220,600,580,600);
+	
+	//							(x	, y  , w  , l  );
+	Square compCube = new Square(300, 0, 200, 200);
+	Square compCube2 = new Square(300, 0, 200, 200);
+	
+	Timer timer = new Timer(20, this);
+
+	LoadingIcon(){
+		//Set up window
+		win = new JFrame("Loading Icon");
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Set Cube locations using move variable:
+		/* Much easier than changing the original variable*/
+		compCube.moveY = 400;
+		compCube2.moveY = 0;
+		
+		dPanel = new DrawingPanel();
+		dPanel.setPreferredSize(new Dimension (panW, panH));
+		
+		setupGUI();
+		
+		win.add(panel);
+		win.pack();
+		win.setLocationRelativeTo(null);
+		win.setVisible(true);
+
+		timer.start();
+	}
+
+	class Line {
+		double x1,y1,x2,y2;
+		double cx,cy;
+		
+		Line(double a, double b, double c, double d) {
+			x1=a; y1=b; x2=c; y2= d;
+			cx = (x1+x2)/2.0;	//Center of x
+			cy = (y1+y2)/2.0;   //Center of y
+		}
+		
+		void draw(Graphics2D g2) {
+			g2.drawLine( (int)x1, (int)y1, (int)x2,  (int)y2);
+		}
+	}
+	
+	JButton btn1;
+	
+	void setupGUI() {
+		
+		panel = new JPanel(new MigLayout("wrap 2, insets 20"));
+		panel.setPreferredSize(new Dimension (panW+50, panH+50));
+		panel.add(dPanel, "span 2");
+		
+		panel.setBackground(new Color(000, 000, 000));
+		
+		btn1 = new JButton("Pause");
+		btn1.addActionListener(new BtnAL());
+		panel.add(btn1, "growx, wrap");
+		
+		
+	}
+
+	//inner class
+	class DrawingPanel extends JPanel{
+
+		DrawingPanel(){
+			this.setBackground(Color.BLACK);
+			this.setPreferredSize(new Dimension(panW, panH));
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {			
+			super.paintComponent(g);
+			
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			
+			g2.setStroke(new BasicStroke(3));
+			g2.setColor(new Color(255,154,0));
+			g2.drawOval(200,195,400,20);
+			g2.setColor(new Color(39,167,216));
+			g2.drawOval(200,585,400,20);
+			
+			compCube.draw(g2);
+			compCube2.draw(g2);
+			
+			g2.setColor(new Color(0, 0, 0));
+			g2.fillRect(0, 0, 800, 200);
+			g2.fillRect(0, 600, 800, 200);
+			g2.fillRect(0, 0, 200, 800);
+			g2.fillRect(600, 0, 200, 800);
+			
+			g2.setStroke(new BasicStroke(3));
+			g2.setColor(new Color(255,154,0));
+			lineTop.draw(g2);
+			g2.setColor(new Color(39,167,216));
+			lineBot.draw(g2);
+		}		
+	}
+	
+	public class BtnAL implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getActionCommand().equals("Pause")) {
+				timer.stop();
+				btn1.setText("UnPause");
+			}
+			if (e.getActionCommand().equals("UnPause")) {
+				timer.start();
+				btn1.setText("Pause");
+			}
+		}
+	}
+
+	//event listener for timer (main class)
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		compCube.move();
+		compCube2.move();
+		dPanel.repaint();
+		win.setTitle("1:  " + compCube.moveY + "    2:  " + compCube2.moveY);
+	}
+}
---
+++ Square.java
@@ -0,0 +1,141 @@
+//Square - Zein Al-Jaradat
+package loadingIcon;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+
+class Square {
+
+	double angle = 0.0;
+	double moveX = 0.0;		//moving in the x direction
+	double moveY = 0.0;	//moving in the y direction
+
+	final double x1, y1, x2, y2;
+	final double cx,cy;
+
+	Square (double posx, double posy, double wx, double ly) {
+		x1 = posx; y1 = posy; x2 = wx; y2 = ly;
+
+		cx = ((x2/2) + x1);		//Center of x
+		cy = ((y2/2) + y1);		//Center of y
+	}
+
+	void draw(Graphics2D g2) {
+		g2.rotate(angle, cx + moveX, cy + moveY);
+
+		g2.setColor(new Color(255,255,255));
+		g2.fillRect( (int)(x1 + moveX), (int)(y1 + moveY), (int)x2,  (int)y2);
+
+		details(x1 + moveX, y1 + moveY, g2);
+
+		if ((y1+moveY) > LoadingIcon.panH-200) {
+			g2.setColor(new Color(255,255,255));
+			g2.fillRect( (int)(x1 + moveX), (int)(y1 + moveY - LoadingIcon.panH), (int)x2,  (int)y2);
+
+			details(x1 + moveX, y1 + moveY - LoadingIcon.panH, g2);
+		}
+
+		g2.rotate(-angle, cx + moveX, cy + moveY);
+	}
+
+	void move () {
+		angle += 0.01;
+
+		//moveX += 0.0;
+		moveY += 10.0;
+
+		if ((y1+moveY) > LoadingIcon.panH) {
+			moveY = 0;
+		}
+	}
+
+	void details (double x, double y, Graphics g) {
+		g.setColor(Color.BLACK);
+		
+		//top
+		Polygon edgeCut = new Polygon();
+		edgeCut.addPoint((int)x+50, (int)y-1);
+		edgeCut.addPoint((int)x+75, (int)y+20);
+		edgeCut.addPoint((int)x+125, (int)y+20);
+		edgeCut.addPoint((int)x+150, (int)y-1);
+		g.fillPolygon(edgeCut);
+		
+		//bottom
+		edgeCut = new Polygon();
+		edgeCut.addPoint((int)x+50, (int)y+201);
+		edgeCut.addPoint((int)x+75, (int)y+180);
+		edgeCut.addPoint((int)x+125, (int)y+180);
+		edgeCut.addPoint((int)x+150, (int)y+201);
+		g.fillPolygon(edgeCut);
+		
+		//right
+		edgeCut = new Polygon();
+		edgeCut.addPoint((int)x+201, (int)y+50);
+		edgeCut.addPoint((int)x+180, (int)y+75);
+		edgeCut.addPoint((int)x+180, (int)y+125);
+		edgeCut.addPoint((int)x+201, (int)y+150);
+		g.fillPolygon(edgeCut);
+		
+		//left
+		edgeCut = new Polygon();
+		edgeCut.addPoint((int)x-1,	(int)y+50);
+		edgeCut.addPoint((int)x+20, (int)y+75);
+		edgeCut.addPoint((int)x+20, (int)y+125);
+		edgeCut.addPoint((int)x-1,	(int)y+150);
+		g.fillPolygon(edgeCut);
+
+		g.setColor(new Color(127, 131, 132)); //Gray
+		g.fillOval((int)x+35, (int)y+35, 130, 130);
+
+		//top two lines
+		g.fillRect((int)x+75, (int)y+20, 5, 25);
+		g.fillRect((int)x+120, (int)y+20, 5, 25);
+
+		//bottom two lines
+		g.fillRect((int)x+75, (int)y+155, 5, 25);
+		g.fillRect((int)x+120, (int)y+155, 5, 25);
+
+		//right two lines
+		g.fillRect((int)x+155, (int)y+75, 25, 5);
+		g.fillRect((int)x+155, (int)y+120, 25, 5);
+
+		//left two lines
+		g.fillRect((int)x+20, (int)y+75, 25, 5);
+		g.fillRect((int)x+20, (int)y+120, 25, 5);
+		
+		g.setColor(Color.WHITE);
+		
+		//middle circle
+		g.fillOval((int)x+62, (int)y+62, 76, 76);
+		
+		//white extensions
+		g.fillRect((int)x+80, (int)y+16, 40, 5); //top
+		g.fillRect((int)x+80, (int)y+179, 40, 5); //bottom
+		g.fillRect((int)x+16, (int)y+80, 5, 40); //left
+		g.fillRect((int)x+179, (int)y+80, 5, 40); //right
+		
+		g.setColor(new Color(240, 149, 172)); //Pink
+		
+		//pink lines
+		g.fillRect((int)x+97, (int)y+35, 6, 27); //top
+		g.fillRect((int)x+97, (int)y+138, 6, 27); //bottom
+		g.fillRect((int)x+35, (int)y+97, 27, 6); //left
+		g.fillRect((int)x+138, (int)y+97, 27, 6); //right
+		
+		//heart
+		g.fillOval((int)x+72, (int)y+78, 28, 28);
+		g.fillOval((int)x+100, (int)y+78, 28, 28);
+		g.fillRect((int)x+80, (int)y+92, 40, 13);
+		
+		Polygon triangle = new Polygon();
+		triangle.addPoint((int)x+73, (int)y+98);
+		triangle.addPoint((int)x+127, (int)y+98);
+		triangle.addPoint((int)x+100, (int)y+125);
+		g.fillPolygon(triangle);
+
+
+	}
+
+}
